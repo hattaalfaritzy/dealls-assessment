e87771baf9cd4c11308d01c4a89af44d7fd0ccd7
hattaalfaritzy <muh.hara16@gmail.com>
2023-07-21 15:58:58 +0700

    fix header n footer

--- src/components/layouts/footer/footer.tsx
+++ src/components/layouts/footer/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
 	return (
 		<footer id='footer'>
 			<div className='flex justify-center items-center w-full bg-primary py-2 hide-mobile'>
-				<span className='text-sm text-white'>© Muh. Hatta Alfaritzy</span>
+				<span className='text-sm text-white'>© Assessment Test - Muh. Hatta Alfaritzy</span>
 			</div>
 		</footer>
 	)
--- src/components/layouts/header/header.tsx
+++ src/components/layouts/header/header.tsx
@@ -9,7 +9,7 @@ export default function HeaderNavbar() {
     return (
         <header className={clsx('header')}>
             <div className='flex flex-row justify-start items-center w-full h-full hide-mobile'>
-               {/* <h1 className='text-2xl font-medium tracking-wider text-white'>Assessment Test - Dealls</h1> */}
+               <h1 className='text-2xl font-medium tracking-wider text-white'>Assessment Test - Dealls</h1>
             </div>
             <div className='flex flex-row justify-between items-center w-full h-full hide-website'>
                 <ImageWithFallback alt='logo-short' src={'/images/logo-short.svg'} className='w-auto h-auto' />
